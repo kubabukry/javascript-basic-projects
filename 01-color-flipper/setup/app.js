590fb78e89bbb879679d9badbc65c70b5668df74
jakub_bukry <jakub_bukry@epam.com>
2025-07-26 15:49:38 +0200

    add color flipper functionality with random color generation and navigation links

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,13 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btnElem = document.querySelector('.btn-hero');
+const spanElem = document.querySelector('.color');
+const bodyElem = document.querySelector('body');
+
+btnElem.addEventListener('click', function (e) {
+    let randomNum = Math.floor(Math.random() * 4);
+    let color = colors[randomNum];
+    bodyElem.style.background = color;
+    spanElem.textContent = color;
+});
+
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,37 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btnElem = document.querySelector('.btn-hero');
+const spanElem = document.querySelector('.color');
+const bodyElem = document.querySelector('body');
+
+bodyElem.style.background = spanElem.textContent;
+
+btnElem.addEventListener('click', function () {
+    let randomColor = generateRandomColor();
+    bodyElem.style.background = randomColor;
+    spanElem.textContent = randomColor;
+});
+
+
+function generateRandomColor () {
+    let hexString = '';
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    
+    const toHex = (c) =>  {
+        let hexNumber = '';
+        let quotient = c;
+        if(c === 0){
+            return '0';
+        }
+        while(quotient > 0){
+            let remainder = quotient % 16;
+            hexNumber = hex[remainder] + hexNumber;
+            quotient = Math.floor(quotient / 16);
+        }
+        return hexNumber;
+    }
+    
+    return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+}
